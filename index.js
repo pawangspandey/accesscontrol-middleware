a01c35f0023d12390a1114e3050ee29f72ddddb3
pawan pandey <pawan4zip@gmail.com>
2018-01-23 18:05:08 +0530

    working middleware

---
+++ index.js
@@ -0,0 +1,96 @@
+
+class AccessControlMiddleware {
+  /**
+   * @param {object} accessControl access control rules object.
+   */
+
+  constructor(accessControl) {
+    this._accessControl = accessControl;
+  }
+
+  /**
+   * check permission
+   * @param {object} params parameters.
+   * @param {string} params.resource resource name.
+   * @param {string} params.action action would be any of create, read, update & delete.
+   * @param {boolean} params.checkOwnerShip flag to check ownership on resource.
+   * @param {Object[]} params.operands operands which will compare to check ownership.
+   * @param {string} params.operands.source source to get operand value (body, params, query, and whichever present in req object).
+   * @param {string} params.operands.key key to get operand value.
+   * @returns {function} middleware to append in express.js route.
+   */
+
+  check ({ resource, action, checkOwnerShip = false, operands = []}) {
+
+    return (req, res, next) => {
+
+      const actions = {};
+      
+      switch (action) {
+  
+        case 'create' : 
+        actions.any = 'createAny';
+        actions.own = 'createOwn';
+        break;
+  
+        case 'update' : 
+        actions.any = 'updateAny';
+        actions.own = 'updateOwn';
+        break;
+  
+  
+        case 'read' : 
+        actions.any = 'readAny';
+        actions.own = 'readOwn';
+        break;
+  
+  
+        case 'delete' : 
+        actions.any = 'deleteAny';
+        actions.own = 'deleteOwn';
+        break;
+  
+        default:
+        return next(new Error('invalid action'));
+        break;
+      }
+
+      // as most passport strategy assign user object to req.
+      const role = req.user.role; 
+      let permission = {};
+
+      permission = this._accessControl.can(role);
+
+      if (checkOwnerShip) {
+
+        if (operands.length !== 2) {
+          return next(new Error('must be two operands to check ownership'))
+        }
+
+        const firstOperand = req[operands[0].source][operands[0].key];
+
+        const secondOperand = req[operands[1].source][operands[1].key];
+
+        if (firstOperand === secondOperand) {
+          permission = permission[actions.own](resource);
+        }
+        else {
+          permission = permission[actions.any](resource);
+        }
+
+      } else {
+        permission = permission[actions.any](resource);
+      }
+
+      if (permission.granted) {
+        return next();
+      } 
+      else {
+        return res.status(403).send();
+      }
+    };
+  }
+
+}
+
+module.exports = AccessControlMiddleware;
---
+++ test/index.js
@@ -0,0 +1,410 @@
+
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon');
+const mockReq =  require('sinon-express-mock').mockReq;
+const mockRes =  require('sinon-express-mock').mockRes;
+
+chai.config.includeStack = true;
+
+
+const AccessControl = require('accesscontrol');
+const grantsObject = require('./fixtures/rules.json');
+const ac = new AccessControl(grantsObject);
+
+const AccessControlMiddleware = require('../index');
+
+const accessControlMiddleware = new AccessControlMiddleware(ac);
+
+
+describe('## next error arguments check', () => {
+  it('## should throw error if invalid action provide', () => {
+    const request = {
+      user : {
+        role : 'admin'
+      }
+    };
+
+    const req = mockReq(request);
+    const res = mockRes();
+    const next = sinon.spy();
+
+    middleware = accessControlMiddleware.check({ resource : 'video', action: 'invalid-action' })(req, res, next);
+
+    expect(next.calledOnce).to.be.equal(true);
+    
+    expect(typeof next.args[0]).to.be.equal('object');
+  })
+
+  it('## should throw error if insufficient operand provides', () => {
+    const request = {
+      user : {
+        role : 'admin'
+      }
+    };
+
+    const req = mockReq(request);
+    const res = mockRes();
+    const next = sinon.spy();
+
+    middleware = accessControlMiddleware.check({ 
+      resource : 'video',
+      action: 'create',
+      checkOwnerShip : true,
+      operands : [],
+    })(req, res, next);
+
+    expect(next.calledOnce).to.be.equal(true);
+    
+    expect(typeof next.args[0]).to.be.equal('object');
+  })
+});
+
+
+describe('## Admin permission check ', () => {
+
+  it('## admin can view any video', () => {
+    const request = {
+      user : {
+        role : 'admin'
+      }
+    };
+
+    const req = mockReq(request);
+    const res = mockRes();
+    const next = sinon.spy();
+
+    middleware = accessControlMiddleware.check({ resource : 'video', action: 'read' })(req, res, next);
+
+    expect(next.calledOnce).to.be.equal(true);
+  })
+
+
+  it('## admin can view his own video', () => {
+    const request = {
+      user : {
+        role : 'admin',
+        _id : '123456'
+      },
+      params : {
+        userId : '123456'
+      }
+    };
+
+    const req = mockReq(request);
+    const res = mockRes();
+    const next = sinon.spy();
+
+    middleware = accessControlMiddleware.check({ 
+      resource : 'video',
+      action: 'read',
+      checkOwnerShip : true,
+      operands : [{ source : 'user', key : '_id' }, { source : 'params', key : 'userId' }]
+     })(req, res, next);
+
+    expect(next.calledOnce).to.be.equal(true);
+  })
+
+
+  it('## admin can create any video', () => {
+    const request = {
+      user : {
+        role : 'admin'
+      }
+    };
+
+    const req = mockReq(request);
+    const res = mockRes();
+    const next = sinon.spy();
+
+    middleware = accessControlMiddleware.check({ resource : 'video', action: 'create' })(req, res, next);
+
+    expect(next.calledOnce).to.be.equal(true);
+  })
+
+  it('## admin can create his own video', () => {
+    const request = {
+      user : {
+        role : 'admin',
+        _id : '123456'
+      },
+      params : {
+        userId : '123456'
+      }
+    };
+
+    const req = mockReq(request);
+    const res = mockRes();
+    const next = sinon.spy();
+
+    middleware = accessControlMiddleware.check({ 
+      resource : 'video',
+      action: 'create',
+      checkOwnerShip : true,
+      operands : [{ source : 'user', key : '_id' }, { source : 'params', key : 'userId' }]
+     })(req, res, next);
+
+    expect(next.calledOnce).to.be.equal(true);
+  })
+
+
+  it('## admin can update any video', () => {
+    const request = {
+      user : {
+        role : 'admin'
+      }
+    };
+
+    const req = mockReq(request);
+    const res = mockRes();
+    const next = sinon.spy();
+
+    middleware = accessControlMiddleware.check({ resource : 'video', action: 'update' })(req, res, next);
+
+    expect(next.calledOnce).to.be.equal(true);
+  })
+
+  it('## admin can update his own video', () => {
+    const request = {
+      user : {
+        role : 'admin',
+        _id : '123456'
+      },
+      params : {
+        userId : '123456'
+      }
+    };
+
+    const req = mockReq(request);
+    const res = mockRes();
+    const next = sinon.spy();
+
+    middleware = accessControlMiddleware.check({ 
+      resource : 'video',
+      action: 'update',
+      checkOwnerShip : true,
+      operands : [{ source : 'user', key : '_id' }, { source : 'params', key : 'userId' }]
+     })(req, res, next);
+
+    expect(next.calledOnce).to.be.equal(true);
+  })
+
+
+
+  it('## admin can delete any video', () => {
+    const request = {
+      user : {
+        role : 'admin'
+      }
+    };
+
+    const req = mockReq(request);
+    const res = mockRes();
+    const next = sinon.spy();
+
+    middleware = accessControlMiddleware.check({ resource : 'video', action: 'delete' })(req, res, next);
+
+    expect(next.calledOnce).to.be.equal(true);
+  })
+
+  it('## admin can update his own video', () => {
+    const request = {
+      user : {
+        role : 'admin',
+        _id : '123456'
+      },
+      params : {
+        userId : '123456'
+      }
+    };
+
+    const req = mockReq(request);
+    const res = mockRes();
+    const next = sinon.spy();
+
+    middleware = accessControlMiddleware.check({ 
+      resource : 'video',
+      action: 'delete',
+      checkOwnerShip : true,
+      operands : [{ source : 'user', key : '_id' }, { source : 'params', key : 'userId' }]
+     })(req, res, next);
+
+    expect(next.calledOnce).to.be.equal(true);
+  })
+
+});
+
+describe('## User permission check ', () => {
+  
+    it('## user cannot view any video', () => {
+      const request = {
+        user : {
+          role : 'user'
+        }
+      };
+  
+      const req = mockReq(request);
+      const res = mockRes();
+      const next = sinon.spy();
+  
+      middleware = accessControlMiddleware.check({ resource : 'video', action: 'read' })(req, res, next);
+  
+      expect(next.calledOnce).to.be.equal(false);
+    })
+  
+  
+    it('## user can view his own video', () => {
+      const request = {
+        user : {
+          role : 'user',
+          _id : '123456'
+        },
+        params : {
+          userId : '123456'
+        }
+      };
+  
+      const req = mockReq(request);
+      const res = mockRes();
+      const next = sinon.spy();
+  
+      middleware = accessControlMiddleware.check({ 
+        resource : 'video',
+        action: 'read',
+        checkOwnerShip : true,
+        operands : [{ source : 'user', key : '_id' }, { source : 'params', key : 'userId' }]
+       })(req, res, next);
+  
+      expect(next.calledOnce).to.be.equal(true);
+    })
+  
+  
+    it('## user cannot create any video', () => {
+      const request = {
+        user : {
+          role : 'user'
+        }
+      };
+  
+      const req = mockReq(request);
+      const res = mockRes();
+      const next = sinon.spy();
+  
+      middleware = accessControlMiddleware.check({ resource : 'video', action: 'create' })(req, res, next);
+  
+      expect(next.calledOnce).to.be.equal(false);
+    })
+  
+    it('## user can create his own video', () => {
+      const request = {
+        user : {
+          role : 'admin',
+          _id : '123456'
+        },
+        params : {
+          userId : '123456'
+        }
+      };
+  
+      const req = mockReq(request);
+      const res = mockRes();
+      const next = sinon.spy();
+  
+      middleware = accessControlMiddleware.check({ 
+        resource : 'video',
+        action: 'create',
+        checkOwnerShip : true,
+        operands : [{ source : 'user', key : '_id' }, { source : 'params', key : 'userId' }]
+       })(req, res, next);
+  
+      expect(next.calledOnce).to.be.equal(true);
+    })
+  
+  
+    it('## user cannot update any video', () => {
+      const request = {
+        user : {
+          role : 'user'
+        }
+      };
+  
+      const req = mockReq(request);
+      const res = mockRes();
+      const next = sinon.spy();
+  
+      middleware = accessControlMiddleware.check({ resource : 'video', action: 'update' })(req, res, next);
+  
+      expect(next.calledOnce).to.be.equal(false);
+    })
+  
+    it('## user can update his own video', () => {
+      const request = {
+        user : {
+          role : 'user',
+          _id : '123456'
+        },
+        params : {
+          userId : '123456'
+        }
+      };
+  
+      const req = mockReq(request);
+      const res = mockRes();
+      const next = sinon.spy();
+  
+      middleware = accessControlMiddleware.check({ 
+        resource : 'video',
+        action: 'update',
+        checkOwnerShip : true,
+        operands : [{ source : 'user', key : '_id' }, { source : 'params', key : 'userId' }]
+       })(req, res, next);
+  
+      expect(next.calledOnce).to.be.equal(true);
+    })
+  
+  
+  
+    it('## user cannot delete any video', () => {
+      const request = {
+        user : {
+          role : 'user'
+        }
+      };
+  
+      const req = mockReq(request);
+      const res = mockRes();
+      const next = sinon.spy();
+  
+      middleware = accessControlMiddleware.check({ resource : 'video', action: 'delete' })(req, res, next);
+  
+      expect(next.calledOnce).to.be.equal(false);
+    })
+  
+    it('## user can update his own video', () => {
+      const request = {
+        user : {
+          role : 'user',
+          _id : '123456'
+        },
+        params : {
+          userId : '123456'
+        }
+      };
+  
+      const req = mockReq(request);
+      const res = mockRes();
+      const next = sinon.spy();
+  
+      middleware = accessControlMiddleware.check({ 
+        resource : 'video',
+        action: 'delete',
+        checkOwnerShip : true,
+        operands : [{ source : 'user', key : '_id' }, { source : 'params', key : 'userId' }]
+       })(req, res, next);
+  
+      expect(next.calledOnce).to.be.equal(true);
+    })
+  
+  });
+  
+
